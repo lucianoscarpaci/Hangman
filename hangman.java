6162b1bc59033bd08708a8e6d5b16c890f455230
lucianoscarpaci <scarpacii.luciano@gmail.com>
2022-07-21 14:44:02 -0400

    Fixed coding error on line 13

--- hangman.java
+++ hangman.java
@@ -1,12 +1,25 @@
+import java.util.List;
 import java.io.FileNotFoundException; //import java file class
 import java.util.Scanner; //import scanner class
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class hangman {
     public static void main(String[] args) throws FileNotFoundException {
 
         Scanner scanner = new Scanner(new java.io.File("./words_alpha.txt"));
 
+        List<String> words = new ArrayList<>();
+
         while (scanner.hasNext()) {
-            System.out.println(scanner.nextLine());
+            words.add(scanner.nextLine());
         }
+
+        Random rand = new Random();
+
+        String word = words.get(rand.nextInt(words.size())); //random number between 0 and list of words
+
+        System.out.println(word);
     }
 }
